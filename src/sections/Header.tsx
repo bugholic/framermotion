04621b595909507ea7898776c876c44f79a66616
bugholic <masterav2001@gmail.com>
2024-11-14 14:16:52 +0530

    added section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,9 +4,10 @@ import { Button } from "@/components/Button";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15">
+    <header className="py-4 border-b border-white/15 sticky top-0 z-10">
+      <div className="absolute inset-0  backdrop-blur -z-10"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto md:backdrop-blur">
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
               <LogoIcon className="w-8 h-8" />
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,27 +1,40 @@
 import { Button } from "@/components/Button";
-import startBg from "@/assets/stars.png"
+import startBg from "@/assets/stars.png";
 
 export const Hero = () => {
   return (
-    <section className="h-[492px] flex items-center overflow-hidden relative" style={{backgroundImage: `url(${startBg.src})`}}>
-        <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] boxShadow"></div>
-        <div className="absolute h-[344px] w-[344px] border opacity-20  rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "></div>
-        <div className="absolute h-2 w-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-        <div className="absolute h-2 w-2 left-1/2 bg-white rounded-full top-0 -translate-x-1/2 -translate-y-1/2"></div>
-        <div className="absolute h-5 w-5 border border-white left-full bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex justify-center items-center">
-        <div className="h-2 w-2 bg-white rounded-full">
-            </div></div>
-            <div className="absolute h-[444px] w-[444px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed">
-            </div>
-            <div className="absolute h-[544px] w-[544px] rounded-full border border-white opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed">
-            </div>
+    <section
+      className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
+      style={{ backgroundImage: `url(${startBg.src})` }}
+    >
+      <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
+      {/* start planet */}
+      <div className="absolute h-64 w-64 md:h-96 md:w-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] boxShadow"></div>
+      {/* end planet */}
+      {/* Start Ring 1 */}
+      <div className="absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] border opacity-20  rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "></div>
+      <div className="absolute h-2 w-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute h-2 w-2 left-1/2 bg-white rounded-full top-0 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute h-5 w-5 border border-white left-full bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex justify-center items-center">
+        <div className="h-2 w-2 bg-white rounded-full"></div>
+      </div>
+      {/* End ring 1 */}
+      {/* start ring 2 */}
+      <div className="absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"></div>
+      {/* end ring 2 */}
+      {/* start ring 3 */}
+      <div className="absolute h-[544px] w-[544px] md:h-[980px] md:w-[980px] rounded-full border border-white opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"></div>
+      {/* end ring 3 */}
       <div className="container relative mt-16">
-        <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">AI SEO</h1>
-        <p className="text-lg text-white/70 mt-5 text-center">
-            Elevate your site's visibility effortlessly with AI, where smart technology meets user-friendly SEO tools.
+        <h1 className="text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+          AI SEO
+        </h1>
+        <p className="text-lg md:text-xl text-white/70 mt-5 text-center max-w-xl mx-auto">
+          Elevate your site's visibility effortlessly with AI, where smart
+          technology meets user-friendly SEO tools.
         </p>
         <div className="flex justify-center mt-5">
-       <Button>Join waitlist</Button>
+          <Button>Join waitlist</Button>
         </div>
       </div>
     </section>
