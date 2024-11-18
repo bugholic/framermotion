167de86a7c6d5133575a620458c7c94b02d9dbd7
bugholic <masterav2001@gmail.com>
2024-11-18 20:40:41 +0530

    updated animation in testimonials

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -5,7 +5,7 @@ import productImage from "@/assets/product-image.png";
 import guageImg from "@/assets/icon-guage.png";
 import clickImg from "@/assets/icon-click.png";
 import starsImg from "@/assets/icon-stars.png";
-import { useEffect, useRef } from "react";
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from "react";
 import {
   animate,
   motion,
@@ -41,7 +41,10 @@ const tabs = [
   },
 ];
 
-const FeatureTab = (tab: (typeof tabs)[number]) => {
+const FeatureTab = (
+  props: (typeof tabs)[number] &
+    ComponentPropsWithoutRef<"div"> & { selected: boolean }
+) => {
   const tabRef = useRef<HTMLDivElement>(null);
   const dotLottieRef = useRef(null);
 
@@ -51,8 +54,12 @@ const FeatureTab = (tab: (typeof tabs)[number]) => {
   const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%,black,transparent)`;
 
   useEffect(() => {
-    if (!tabRef.current) return;
-    const { height, width } = tabRef.current.getBoundingClientRect();
+    if (!tabRef.current || !props.selected) return;
+
+    xPercentage.set(0);
+    yPercentage.set(0);
+
+    const { height, width } = tabRef.current?.getBoundingClientRect();
 
     const circumference = height * 2 + width * 2;
 
@@ -73,30 +80,32 @@ const FeatureTab = (tab: (typeof tabs)[number]) => {
     };
 
     animate(xPercentage, [0, 100, 100, 0, 0], options);
-    animate(xPercentage, [0, 0, 100, 100, 0], options);
+    animate(yPercentage, [0, 0, 100, 100, 0], options);
   }, []);
 
   const handleTabHover = () => {
     if (dotLottieRef.current === null) return;
-
   };
   return (
     <div
       ref={tabRef}
       onMouseEnter={handleTabHover}
       className="relative border border-white/15 rounded-xl p-2.5 flex items-center gap-2.5 lg:flex-1"
+      onClick={props.onClick}
     >
-      <motion.div
-        style={{
-          maskImage,
-        }}
-        className="absolute inset-0 border border-[#A369FF] -m-px rounded-xl"
-      ></motion.div>
+      {props.selected && (
+          <motion.div
+            style={{
+              maskImage,
+            }}
+            className="absolute inset-0 border border-[#A369FF] -m-px rounded-xl"
+          ></motion.div>
+      )}
       <div className="border border-white/15 p-2.5 rounded-lg inline-flex items-center w-12 h-12">
-        <Image width={40} height={8} src={tab.icon} alt="icon" />
+        <Image width={40} height={8} src={props.icon} alt="icon" />
       </div>
-      <div className="p-2.5">{tab.title}</div>
-      {tab.isNew && (
+      <div className="p-2.5">{props.title}</div>
+      {props.isNew && (
         <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
           new
         </div>
@@ -106,6 +115,42 @@ const FeatureTab = (tab: (typeof tabs)[number]) => {
 };
 
 export const Features = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const backgroundPositionX = useMotionValue(tabs[0].backgroundPositionX);
+  const backgroundPositionY = useMotionValue(tabs[0].backgroundPositionY);
+  const backgroundSizeX = useMotionValue(tabs[0].backgroundSizeX);
+
+  const backgroundPosition = useMotionTemplate`${backgroundPositionX}% ${backgroundPositionY}%`;
+  const backgroundSize = useMotionTemplate`${backgroundSizeX}% auto`;
+
+  const handleSelectedTab = (index: number) => {
+    setSelectedTab(index);
+
+    const animateOptions: ValueAnimationTransition = {
+      duration: 2,
+      ease: "easeInOut",
+    };
+
+    animate(
+      backgroundSizeX,
+      [backgroundSizeX.get(), 100, tabs[index].backgroundSizeX],
+      animateOptions
+    );
+
+    animate(
+      backgroundPositionX,
+      [backgroundPositionX.get(), tabs[index].backgroundPositionX],
+      animateOptions
+    );
+
+    animate(
+      backgroundPositionY,
+      [backgroundPositionY.get(), tabs[index].backgroundPositionY],
+      animateOptions
+    );
+  };
+
   return (
     <section className="container py-20 md:py-24">
       <h2 className="text-5xl md:text-6xl text-center tracking-tighter">
@@ -116,17 +161,24 @@ export const Features = () => {
         revolutionized the way businesses approach SEO.
       </p>
       <div className="mt-10 flex flex-col lg:flex-row gap-3 ">
-        {tabs.map((tab) => (
-          <FeatureTab {...tab} key={tab.title} />
+        {tabs.map((tab, tabIndex) => (
+          <FeatureTab
+            {...tab}
+            selected={selectedTab === tabIndex}
+            onClick={() => handleSelectedTab(tabIndex)}
+            key={tab.title}
+          />
         ))}
       </div>
       <div className="rounded-xl mt-3 p-2.5 border border-white/20">
-        <div
+        <motion.div
           className="aspect-video bg-cover border border-white/20 rounded-lg"
           style={{
+            backgroundPosition,
+            backgroundSize,
             backgroundImage: `url(${productImage.src})`,
           }}
-        ></div>
+        ></motion.div>
       </div>
     </section>
   );
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,12 @@
+'use client'
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
+import { motion } from "framer-motion";
 import Image from "next/image";
+
+
 
 const testimonials = [
   {
@@ -42,32 +46,46 @@ export const Testimonials = () => {
           Our revolutionary AI SEO tools have transformed our
           clients'strategies.
         </p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-        <div className="flex gap-5">
-          {testimonials.map((testimonial) => (
-            <div
-              key={testimonial.name}
-              className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
-            >
-              <div className="text-lg tracking-tight md:text-2xl">{testimonial.text}</div>
-              <div className="flex items-center gap-3 mt-5">
-                <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
-                  <Image
-                    src={testimonial.avatarImg}
-                    alt={`Avatar for ${testimonial.name}`}
-                    className="h-11 w-11 rounded-lg grayscale"
-                  />
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)] ">
+          <motion.div
+            initial= {{
+              translateX: "-50%"
+            }}
+            animate={{
+              translateX:"0"
+            }}
+            transition={{
+              repeat:Infinity,
+              ease:'linear',
+              duration:30
+            }}
+          className="flex gap-5 flex-none pr-5">
+            {[...testimonials, ...testimonials].map((testimonial, index) => (
+              <div
+                key={index}
+                className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
+              >
+                <div className="text-lg tracking-tight md:text-2xl">
+                  {testimonial.text}
                 </div>
-                <div className="flex flex-col">
-                  <div>{testimonial.name}</div>
-                  <div className="text-white/50 text-sm">
-                    {testimonial.title}
+                <div className="flex items-center gap-3 mt-5">
+                  <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
+                    <Image
+                      src={testimonial.avatarImg}
+                      alt={`Avatar for ${testimonial.name}`}
+                      className="h-11 w-11 rounded-lg grayscale"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <div>{testimonial.name}</div>
+                    <div className="text-white/50 text-sm">
+                      {testimonial.title}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-          </div>
+            ))}
+          </motion.div>
         </div>
       </div>
     </section>
